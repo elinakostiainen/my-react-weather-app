f3b520f0b70ecf58ff92eeab2231ce7550cebea7
Elina Kostiainen <elinakostiainen@Elinas-MacBook-Air.local>
2022-02-17 13:50:09 +1000

    Fixed error by returning null

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,6 +24,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
